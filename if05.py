a4db7eb0bb2c81debc9cc8eb37dacd2987c18e04
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-17 11:47:20 +0500

    5

--- if05.py
+++ if05.py
@@ -6,4 +6,21 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    "78945"
+    a=n//10000
+    b=n//1000%10
+    c=n//100%100%10
+    d=n%1000//10%10
+    e=n%10000%1000%100%10
+    m=a
+    if b>m:
+        m=b
+    if c>m:
+        m=c
+    if d>m:
+        m=d
+    if e>m:
+        m=e
+    return m
+n=int(input("5 xonali son  "))
+print(main(n))
