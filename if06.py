135a4b5d4f3f175c967acd3a07b807c7ea1f2e6b
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-17 11:47:38 +0500

    6

--- if06.py
+++ if06.py
@@ -6,4 +6,31 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=n//1000%10
+    c=n//100%100%10
+    d=n%1000//10%10
+    e=n%10000%1000%100%10
+    m=a
+    index =5
+    if b>m:
+        m=b
+        index =4
+
+    if c>m:
+        m=c
+        index = 3
+
+    if d>m:
+        m=d
+        index=2
+    if e>m:
+        m=e
+        index=1
+    return index
+n=int(input("5 xonali son  "))
+print(main(n))
+
+
+
+
