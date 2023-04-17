2c2110f534a2fc8a8ac5a63028d2c32e5c74caa8
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-04-17 09:50:32 +0500

    2

--- if02.py
+++ if02.py
@@ -8,4 +8,19 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    e=0
+    if a<b<c or a<c<b:
+        e=a
+    
+    if b<a<c or b<c<a:
+        e=b
+    
+    if c<b<a or c<a<b:
+        e=c
+
+
+    return e
+a=int(input("son kirit "))
+b=int(input("son kirit "))
+c=int(input("son kirit "))
+print(main(a,b,c))
